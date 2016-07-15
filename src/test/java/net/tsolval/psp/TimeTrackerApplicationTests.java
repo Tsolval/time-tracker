8047099c9c450fefec1874c82cd06db50f5747ae
Walker D. Adams <tsolval@gmail.com>
2016-07-15 13:44:30 -0500

    initialize project
    used https://start.spring.io to initialize a gradle and maven project and combined them to create this commit.

---
+++ src/test/java/net/tsolval/psp/TimeTrackerApplicationTests.java
@@ -0,0 +1,18 @@
+package net.tsolval.psp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TimeTrackerApplication.class)
+@WebAppConfiguration
+public class TimeTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
