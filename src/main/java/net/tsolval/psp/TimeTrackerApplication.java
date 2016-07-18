5eee561b83ae5053407b0bd79613798ceb29883d
Walker D. Adams <tsolval@gmail.com>
2016-07-18 13:17:00 -0500

    Add JavaDocs and Remove unnecessary Tests
    TimeTrackerApplicationTests was an integration test that just verified
    the context was built correctly.  The new integration test in the
    integration test folder will do this as well, but also verifies the
    JobController code.  This test was therefore removed as redundant.

--- src/main/java/net/tsolval/psp/TimeTrackerApplication.java
+++ src/main/java/net/tsolval/psp/TimeTrackerApplication.java
@@ -3,9 +3,19 @@ package net.tsolval.psp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application configuration class.  Add or reference Spring configurations here.
+ *
+ * @author tsolval
+ */
 @SpringBootApplication
 public class TimeTrackerApplication {
 
+	/**
+	 * This is the main starting point for the application.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTrackerApplication.class, args);
 	}
